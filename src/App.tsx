45e64cd0e5e5add4f63b24f4c9311493f0d56078
Kap Thang <73055456+thangk@users.noreply.github.com>
2022-07-15 00:59:30 -0400

    updated react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './styles/App.module.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Products from './pages/Products';
 import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
 
       <Routes>
         {/* copy the following method to add your page after adding the file in "pages" folder */}
-        <Route path='/' element={<Products />} />
-        <Route path='/' element={<ErrorPage />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
 
       {/* place to add footer */}
