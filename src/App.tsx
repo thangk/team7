97d7c8748084e464e463e41ad78b9fd0bd9ef1a9
Kap Thang <73055456+thangk@users.noreply.github.com>
2022-07-15 00:09:53 -0400

    some example usage of tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import './styles/App.module.scss';
 
 function App() {
   return (
-    <h1>Welcome to Team 7's frontend</h1>
+    <>
+      <h1 className='top-title'>Welcome to Team 7's frontend</h1>
+      <h3 className='bg-red-400 text-4xl'>A subtitle text</h3>
+    </>
   );
 }
 
