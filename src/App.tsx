5d27d9b805df5a9c3d10c2de978c197ed7399d6e
carnevap <carnevap@uwindsor.ca>
2022-07-20 22:26:34 -0400

    Started login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/App.module.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Products from './pages/Products';
+import LoginPage from './pages/LoginPage';
 import ErrorPage from './pages/ErrorPage';
 import Home from './pages/Home';
 import Products_DetailsPage from './components/Products_DetailsPage';
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         {/* copy the following method to add your page after adding the file in "pages" folder */}
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/products" element={<Products />} />
         <Route path="*" element={<ErrorPage />} />
         <Route path="/ProductDetails" element={<Products_DetailsPage/>} /> {/*Temporarily added to access product details*/}
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
-export default function loginPage() {
+export default function LoginPage() 
+{
   return (
     <>
-        <div>loginPage</div>
+      <div>LoginPage *header goes here*</div>
+      <form action="action_page.php" method="post">
+        <div className="imgcontainer">
+        </div>
+        <div className="container">
+          <label htmlFor="uname"><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" name="uname" required/>
+          <label htmlFor="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required/>
+          <button type="submit">Login</button>
+          <label>
+          <input type="checkbox" name="remember"/> Remember me
+          </label>
+        </div>
+        <div className="container">
+          <button type="button" className="cancelbtn">Cancel</button>
+          <span className="psw">Forgot <a href="#">password?</a></span>
+        </div>
+      </form>
     </>
   )
 }
