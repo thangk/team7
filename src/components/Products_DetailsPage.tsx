fdf55905882f93e4d05f23c563bf9acce1d3a2fd
keanu <keonk87@icloud.com>
2022-07-20 22:26:56 -0400

    worked on product details

--- src/components/Products_DetailsPage.tsx
+++ src/components/Products_DetailsPage.tsx
@@ -1,28 +1,15 @@
 import React, { useRef, useState } from 'react'
-
 import { motion, useCycle } from 'framer-motion'
-// import logo from '../images/icons/logo-icon.png'
-// import account from '../images/icons/account-icon.png'
-import logo2 from '../images/icons/logo2-icon.png'
-import data from '../assets/template.json'
 import noimage from '../images/icons/noimage.jpg'
-// import axios from 'axios'
-import ReactPaginate from 'react-paginate'
-import { nanoid } from 'nanoid'
-import { AiFillShopping, AiOutlineCaretLeft, AiOutlineCaretRight, AiOutlineUser } from 'react-icons/ai'
-import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa'
-import ProductsFilterPanel from '../components/ProductsFilterPanel'
-import { MenuToggle } from '../components/MenuToggle'
-import { useDimensions } from '../customHooks/useDimension'
-import { MenuList } from '../components/MenuList'
+
 
 function Products_DetailsPage() {
   return (
     
     <>
-      <h1 className='page-title'>Product Title</h1>
-
       <div className='product-details-wrapper'>
+        <h1 className='page-title'>Product Title</h1>
+
         <div className='product-details-image'>
             <img src={noimage} alt='noimage' />
           </div>
