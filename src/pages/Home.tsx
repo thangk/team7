58a3dfc5f5f5d780836c47b303c468ee395475e1
Adam Taylor <adam.taylor0147@gmail.com>
2022-07-26 22:04:36 -0400

    Home Page Adjustments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import watch from "../images/icons/watch.jpg";
 import React from 'react'
 
 function Home() {
   return (
     //@ts-ignore
-    <h1>I changed the home page text</h1>
+    
+    <>
+      <br></br>
+      <br></br>
+      <h1 className="page-title-center">"Revolutionizing the way you Shop for Watches"</h1>
+      <br></br>
+      <br></br>
+      <div className="background-image">
+        <img src={watch} alt="watch" />
+      </div>
+      <br></br>
+      <br></br>
+      <h1 className="page-title-center">Newest Additions</h1>
+      </>
     //Adding comment to test pull request
   )
 }
