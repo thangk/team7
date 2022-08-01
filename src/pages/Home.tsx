f369e0aa14d2a887e613844c16ad858d7b3177dd
Adam Taylor <adam.taylor0147@gmail.com>
2022-07-31 20:56:58 -0400

    Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,54 +8,122 @@ import { nanoid } from "nanoid";
 
 function Home() {
 
-  const navigate = useNavigate();
-  return (
+const navigate = useNavigate();
+return (
     
-      <main className="homepage-wrapper">
+  <main className="homepage-wrapper">
 
-        <h1 className="page-title-center">"Revolutionizing the way you Shop for Watches"</h1>
+    <h1 className="page-title-center">"Revolutionizing the way you Shop for Watches"</h1>
   
   
-        <div className="background-wrapper">
-          <img src={watch} alt="watch" />
-        </div>
+    <div className="background-wrapper">
+      <img src={watch} alt="watch" />
+    </div>
+    <h1 className="page-title-center">Newest Additions</h1>
 
-        <h1 className="page-title-center">Newest Additions</h1>
+    {/*Newest addition listings*/}
+    <section className="home-products-panel">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        key={nanoid()}
+        className="product-cards-wrapper"
+      >         
+      <motion.div
+        className="product-card"
+        onClick={() => {
+        navigate(`/${data[0].id}`); console.log(`/${data[0].id}`)
+        }}
+        whileHover={{ y: -10 }}
+      >
+      <div className="product-card-image">
+        <img src={noimage} alt="noimage" />
+      </div>
+      <div className="product-card-text">
+        <div>Name: {data[0].name}</div>
+        <div>Brand: {data[0].brand}</div>
+        <div>Price: {data[0].price}</div>
+        <div>ID: {data[0].id}</div>
+      </div>
+      </motion.div>
+    </motion.div>
 
-        {/*Newest addition listings*/}
+    <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        key={nanoid()}
+        className="product-cards-wrapper"
+      >         
+      <motion.div
+        className="product-card"
+        onClick={() => {
+        navigate(`/${data[1].id}`); console.log(`/${data[1].id}`)
+        }}
+        whileHover={{ y: -10 }}
+      >
+      <div className="product-card-image">
+        <img src={noimage} alt="noimage" />
+      </div>
+      <div className="product-card-text">
+        <div>Name: {data[1].name}</div>
+        <div>Brand: {data[1].brand}</div>
+        <div>Price: {data[1].price}</div>
+        <div>ID: {data[1].id}</div>
+      </div>
+      </motion.div>
+    </motion.div>
 
-        
-        <section className="home-products-panel">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          key={nanoid()}
-          className="product-cards-wrapper"
-        >         
+    <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        key={nanoid()}
+        className="product-cards-wrapper"
+      >         
+      <motion.div
+        className="product-card"
+        onClick={() => {
+        navigate(`/${data[2].id}`); console.log(`/${data[2].id}`)
+        }}
+        whileHover={{ y: -10 }}
+      >
+      <div className="product-card-image">
+        <img src={noimage} alt="noimage" />
+      </div>
+      <div className="product-card-text">
+        <div>Name: {data[2].name}</div>
+        <div>Brand: {data[2].brand}</div>
+        <div>Price: {data[2].price}</div>
+        <div>ID: {data[2].id}</div>
+      </div>
+      </motion.div>
+    </motion.div>
 
-        <motion.div
-          className="product-card"
-          onClick={() => {
-            navigate(`/${data[0].id}`); console.log(`/${data[0].id}`)
-          }}
-          whileHover={{ y: -10 }}
-        >
-
-        <div className="product-card-image">
-          <img src={noimage} alt="noimage" />
-        </div>
-        <div className="product-card-text">
-          <div>Name: {data[0].name}</div>
-          <div>Brand: {data[0].brand}</div>
-          <div>Price: {data[0].price}</div>
-          <div>ID: {data[0].id}</div>
-        </div>
-            </motion.div>
-          </motion.div>
-        </section>
-
-
-      </main>
+    <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        key={nanoid()}
+        className="product-cards-wrapper"
+      >         
+      <motion.div
+        className="product-card"
+        onClick={() => {
+        navigate(`/${data[3].id}`); console.log(`/${data[3].id}`)
+        }}
+        whileHover={{ y: -10 }}
+      >
+      <div className="product-card-image">
+        <img src={noimage} alt="noimage" />
+      </div>
+      <div className="product-card-text">
+        <div>Name: {data[3].name}</div>
+        <div>Brand: {data[3].brand}</div>
+        <div>Price: {data[3].price}</div>
+        <div>ID: {data[3].id}</div>
+      </div>
+      </motion.div>
+    </motion.div>
+  </section>
+</main>
 
 
 
