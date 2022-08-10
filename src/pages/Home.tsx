799b166cb8e471a051aa90f398a54f49cb705e6c
SpaceCadet8716 <60045671+SpaceCadet8716@users.noreply.github.com>
2022-08-10 14:22:12 -0400

    Newly added watches now appear on homescreen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import watch from "../images/icons/watch.jpg";
-import React from 'react';
-import data from "../assets/template.json";
+import React, { useEffect, useState } from 'react';
 import noimage from "../images/icons/noimage.jpg";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
@@ -10,13 +9,33 @@ import { useSelector } from 'react-redux'
 import iwlogo from '../images/icons/logo-icon-small.png'
 // @ts-ignore
 import { Helmet } from 'react-helmet-async'
+import api from '../api/base';
+import defaultWatches from "../assets/defaultWatches.json";
 
 function Home() {
 
   // @ts-ignore
   const currentTheme = useSelector(state => state.theme.current)
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  //@ts-ignore
+  const [watches, setWatches] = useState(defaultWatches);
+
+  useEffect(() => {
+    async function getWatches() {
+      try {
+        const {data} = await api.get('/watches/new-releases')
+        setWatches(data)
+      } catch {
+        console.log("Failed to retrieve newest edition watches")
+      }
+    }
+
+    getWatches()
+
+  }, []) //[] is put as the second param so useEffect only runs once
+
 return (
     
   <main className="homepage-wrapper">
@@ -61,17 +80,18 @@ return (
       <motion.div
         className={`home-product-card theme-bg-${currentTheme}`}
         onClick={() => {
-        navigate(`/${data[0].id}`); console.log(`/${data[0].id}`)
+        {/*@ts-ignore*/}
+        navigate(`products/${watches[0].id}`); console.log(`products/${watches[0].id}`)
         }}
         whileHover={{ y: -10 }}
       >
       <div className={`home-product-card-image theme-border-${currentTheme}-light`}>
-        <img src={noimage} alt="noimage" />
+        <img src={watches[0].imageUrl} alt="noimage" />
       </div>
       <div className={`home-product-card-text theme-border-${currentTheme}`}>
-        <div>Name: {data[0].name}</div>
-        <div>Brand: {data[0].brand}</div>
-        <div>Price: {data[0].price}</div>
+        <div>Name: {watches[0].name}</div>
+        <div>Brand: {watches[0].brand}</div>
+        <div>Price: {watches[0].price}</div>
         
       </div>
       </motion.div>
@@ -85,17 +105,18 @@ return (
       <motion.div
         className={`home-product-card theme-bg-${currentTheme}`}
         onClick={() => {
-        navigate(`/${data[1].id}`); console.log(`/${data[1].id}`)
+        {/*@ts-ignore*/}
+        navigate(`products/${watches[1].id}`); console.log(`products/${watches[1].id}`)
         }}
         whileHover={{ y: -10 }}
       >
       <div className={`home-product-card-image theme-border-${currentTheme}-light`}>
-        <img src={noimage} alt="noimage" />
+        <img src={watches[1].imageUrl} alt="noimage" />
       </div>
       <div className={`home-product-card-text theme-border-${currentTheme}`}>
-        <div>Name: {data[1].name}</div>
-        <div>Brand: {data[1].brand}</div>
-        <div>Price: {data[1].price}</div>
+        <div>Name: {watches[1].name}</div>
+        <div>Brand: {watches[1].brand}</div>
+        <div>Price: {watches[1].price}</div>
       </div>
       </motion.div>
     </motion.div>
@@ -108,17 +129,18 @@ return (
       <motion.div
         className={`home-product-card theme-bg-${currentTheme}`}
         onClick={() => {
-        navigate(`/${data[2].id}`); console.log(`/${data[2].id}`)
+        {/*@ts-ignore*/}
+        navigate(`products/${watches[2].id}`); console.log(`products/${watches[2].id}`)
         }}
         whileHover={{ y: -10 }}
       >
       <div className={`home-product-card-image theme-border-${currentTheme}-light`}>
-        <img src={noimage} alt="noimage" />
+        <img src={watches[2].imageUrl} alt="noimage" />
       </div>
       <div className={`home-product-card-text theme-border-${currentTheme}`}>
-        <div>Name: {data[2].name}</div>
-        <div>Brand: {data[2].brand}</div>
-        <div>Price: {data[2].price}</div>
+        <div>Name: {watches[2].name}</div>
+        <div>Brand: {watches[2].brand}</div>
+        <div>Price: {watches[2].price}</div>
       </div>
       </motion.div>
     </motion.div>
@@ -131,17 +153,18 @@ return (
       <motion.div
         className={`home-product-card theme-bg-${currentTheme}`}
         onClick={() => {
-        navigate(`/${data[3].id}`); console.log(`/${data[3].id}`)
+        {/*@ts-ignore*/}
+        navigate(`products/${watches[3].id}`); console.log(`products/${watches[3].id}`)
         }}
         whileHover={{ y: -10 }}
       >
       <div className={`home-product-card-image theme-border-${currentTheme}-light`}>
-        <img src={noimage} alt="noimage" />
+        <img src={watches[3].imageUrl} alt="noimage" />
       </div>
       <div className={`home-product-card-text theme-border-${currentTheme}`}>
-        <div>Name: {data[3].name}</div>
-        <div>Brand: {data[3].brand}</div>
-        <div>Price: {data[3].price}</div>
+        <div>Name: {watches[3].name}</div>
+        <div>Brand: {watches[3].brand}</div>
+        <div>Price: {watches[3].price}</div>
       </div>
       </motion.div>
     </motion.div>
