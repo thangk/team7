f2afbb7b71ab093cded893ff267b7a64a5e92a1a
Adam Taylor <adam.taylor0147@gmail.com>
2022-08-10 02:38:23 -0400

    Contact Us and Find Store done

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -13,11 +13,20 @@ function ContactUs(){
 
     return (
         
-        <main className="find-store-wrapper">
-            <h1 className={`find-store-page-title theme-text-${currentTheme}-1`}>Find a Store</h1>
+        <main className="contact-us-wrapper">
+            <h1 className={`contact-us-page-title theme-text-${currentTheme}-1`}>Contact Us</h1>
             {/* added theme-font-2 and border colour -- Kap */}
-            <div className={`find-store-content-wrapper theme-text-${currentTheme}-1 theme-border-${currentTheme}-light theme-font-2`}>
+            <div className={`contact-us-content-wrapper theme-text-${currentTheme}-1 theme-border-${currentTheme}-light theme-font-2`}>
                 
+                <p>
+                If you have any questions or concerns regarding Infinity watches website, or the company in general,
+                feel free to contact us in any of the forms below. Please understand that our staff in entirely human.
+                You will not recieve an automated response, due to this you may experience longer wait times.
+                </p>
+                <p>There is also a FAQ page that you can view and might find similar problems there</p>
+                <p>Phone Number: 234 543 8472</p>
+                <p>Email Address: InfinityWatchesSupport@InfinityWatches.ca</p>
+                <p>Mailing Address: 123 Cool St - Postal Code: N67J4F</p>
                 </div>
                 
                 
--- src/pages/FindStore.tsx
+++ src/pages/FindStore.tsx
@@ -11,15 +11,7 @@ function FindStore(){
         // University of Windsor's coordinates
         const defaultCoords = { lat: 42.307684, lng: -83.068468 }
 
-        // setup map
-        const render = (status: Status) => {
-            return <h1>{status}</h1>;
-          };
-          
-          <Wrapper apiKey={"YOUR_API_KEY"} render={render}>
-            
-          </Wrapper>
-
+        
     
 
 
@@ -30,10 +22,8 @@ function FindStore(){
             <h1 className={`find-store-page-title theme-text-${currentTheme}-1`}>Find a Store</h1>
             {/* added theme-font-2 and border colour -- Kap */}
             <div className={`find-store-content-wrapper theme-text-${currentTheme}-1 theme-border-${currentTheme}-light theme-font-2`}>
-                
-
-                </div>
-                
+            <iframe style={{width: "100%", height: "600px"}} src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=university%20of%20windsor%20Windsor+(Infinity%20Watches%20Headquarters)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            </div>
                 
 
 
